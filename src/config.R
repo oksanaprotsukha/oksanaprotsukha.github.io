9499b712ceec26a1313226c35c5eda47815151f4
oksanaprotsukha <ok.protsukha@gmail.com>
2024-03-23 00:52:01 -0700

    improved color palette of the dumbell and sunburst charts; increase fonts of the dumbell chart

--- src/config.R
+++
@@ -1,6 +0,0 @@
-# config.R
-
-# API keys
-#Sys.setenv(NREL_API_KEY = "0APNTzIAx0zSQp1duwLZqnINggCKuAl7iTZwrnDL")
-NREL_API_KEY <- "0APNTzIAx0zSQp1duwLZqnINggCKuAl7iTZwrnDL"
-cns_api_key <- "cd8b0910b52b8e1d0609183e0bdeff4b5baa6370"
--- src/filepath.R
+++
@@ -1,3 +0,0 @@
-
-here::set_here(path='/Users/oksi/workspace/evse_equity/eds240_HW4/', verbose = TRUE)
-
